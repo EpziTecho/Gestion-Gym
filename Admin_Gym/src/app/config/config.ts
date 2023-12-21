6a8c28725f0adc230747861f9dee26d075da0200
Sergio Alexander Huayllas Tirado <aogiri98a@gmail.com>
2023-12-20 21:13:07 -0500

    Add file replacements in angular.json

---
+++ Admin_Gym/src/app/config/config.ts
@@ -0,0 +1,4 @@
+import {environment} from 'src/environments/environment.development';
+export const  URL_BACKEND = environment.URL_BACKEND;
+export const  URL_SERVICIOS = environment.URL_SERVICIOS;
+export const  URL_FRONTEND = environment.URL_FRONTEND;
