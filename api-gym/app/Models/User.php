f9b9f2f88a98c7a9a2b5ef597bd9c7d51435e9ee
Sergio Alexander Huayllas Tirado <aogiri98a@gmail.com>
2023-12-22 18:26:09 -0500

    Add Spatie Laravel Permission package

--- api-gym/app/Models/User.php
+++ api-gym/app/Models/User.php
@@ -5,12 +5,14 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject{
     use HasApiTokens, HasFactory, Notifiable;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
